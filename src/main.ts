eb7af452a3d60386541feaf0ffadd13f28b11f03
ibtisamt22 <ibtisam@smartenergypay.com>
2025-06-27 00:42:30 +0200

    packagejson

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,5 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+//    "format": "prettier --write \"src/**/*.ts\" \"test/**/*.ts\"",
+//"start": "nest start",
